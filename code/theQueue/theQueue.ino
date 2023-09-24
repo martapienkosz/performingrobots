0f3ea44b52529065c6626f25f2b002ac6f6a4f17
martapienkosz <70956012+martapienkosz@users.noreply.github.com>
2023-09-24 20:13:38 +0400

    update

---
+++ code/theQueue/theQueue.ino
@@ -0,0 +1,79 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+}
+
+void loop() {
+
+  for (int i = 0; i < 3; i = i+1) {
+    backward(500); // 0.5m
+    stop(2000);
+  }
+
+  turnClockwise(300);
+  stop(1000);
+  backward(300); // 0.5m
+  stop(2000);
+  turnClockwise(300);
+  stop(1000);
+
+  for (int i = 0; i < 2; i = i+1) {
+    backward(500); // 0.5m
+    stop(2000);
+  }
+
+  antiClockwise(300);
+  stop(1000);
+  backward(300); // 0.5m
+  stop(2000);
+  antiClockwise(300);
+  stop(1000);
+
+  for (int i = 0; i < 2; i = i+1) {
+    backward(500); // 0.5m
+    stop(2000);
+  }
+}
+
+void turnClockwise (int duration) {
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);
+  delay(duration);
+}
+
+void antiClockwise (int duration) {
+  digitalWrite(3, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  delay(duration);
+}
+
+void stop(int duration) {
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  delay(duration);
+}
+
+void forward (int duration) {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(duration);
+}
+
+void backward (int duration) {
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, LOW);
+  delay(duration);
+}
